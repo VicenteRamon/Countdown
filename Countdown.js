bf5639e813e1e0e22d94e02f6996c4002280d8f5
Ramon Vicente <vicente.silva.ramon@gmail.com>
2022-10-05 22:43:29 -0300

    Trololo video added

--- Countdown.js
+++ Countdown.js
@@ -1,28 +1,48 @@
 function updateTimer() {
+  launch = Date.parse("december 05, 2022 22:23:00");
+  now = new Date();
 
-launch = Date.parse("october 02, 2022 12:00:00");
-now = new Date();
+  count = launch - now;
 
-count = launch - now;
+  days = Math.floor(count / (1000 * 60 * 60 * 24));
+  hours = Math.floor(count / (1000 * 60 * 60));
+  mins = Math.floor(count / (1000 * 60));
+  secs = Math.floor(count / 1000);
 
-days = Math.floor(count / (1000 * 60 * 60 * 24));
-hours = Math.floor(count / (1000 * 60 * 60));
-mins = Math.floor(count / (1000 * 60));
-secs = Math.floor(count / 1000);
+  Dias = days;
+  Horas = hours - days * 24;
+  Minutos = mins - hours * 60;
+  Segundos = secs - mins * 60;
 
-Dias = days;
-Horas = hours - days * 24;
-Minutos = mins - hours * 60;
-Segundos = secs - mins * 60;
+  const end = count <= 0 ? menss() : timer();
+
+  function menss() {
+    document.getElementById("timer").innerHTML =
+      '<iframe width="560" height="315" src="https://www.youtube.com/embed/oavMtUWDBTM" title="Trololo" frameborer="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    
+      stop();
+    }
 
 
-document.getElementById("timer")
-  .innerHTML =
-  '<div><span>Dias</span>' + Dias + '</div>' + '<div>:</div>' +
-  '<div><span>Horas</span>' + Horas + '</div>' +'<div>:</div>' +
-  '<div><span>Minutos</span>' + Minutos + '</div>' +'<div>:</div>' +
-  '<div><span>Segundos</span>' + Segundos + '</div>';
-
+  function timer() {
+    document.getElementById("timer").innerHTML =
+      "<div><span>Dias</span>" +
+      Dias +
+      "</div>" +
+      "<div>:</div>" +
+      "<div><span>Horas</span>" +
+      Horas +
+      "</div>" +
+      "<div>:</div>" +
+      "<div><span>Minutos</span>" +
+      Minutos +
+      "</div>" +
+      "<div>:</div>" +
+      "<div><span>Segundos</span>" +
+      Segundos +
+      "</div>";
+  }
 }
+function stop() {clearInterval(interval);}
+const interval = setInterval("updateTimer()", 1000);
 
-setInterval('updateTimer()', 1000);
